51c1e69bec30d6cf43b7afbe624fd19d122c266d
Benjamin Kowalski <Benjamin.Kowalski.1987@gmail.com>
2019-08-05 10:13:05 +0200

    Fix offline bug within service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,32 +1,41 @@
-addEventListener('install', e => {
-	const preCache = async () => {
-		const cache = await caches.open('static')
-		return cache.addAll([
-			'/',
-			'/index.html',
-			'/global.css',
-			'/bundle.css',
-			'/firebase-app.js',
-			'/firebase-auth.js',
-			'/firebase-firestore.js',
-			'/bundle.js',
+const REQUIRED_FILES = [
+	'/',
+	'/index.html',
+	'/global.css',
+	'/bundle.css',
+	'/firebase-app.js',
+	'/firebase-auth.js',
+	'/firebase-firestore.js',
+	'/bundle.js',
+	'/manifest.json',
+	'/icons/icon-512x512.png',
 
-			'/fonts/overpass-regular-webfont.woff2',
-			'/fonts/vollkorn-webfont.woff2',
-			'/fonts/overpass-semibold-webfont.woff2',
-			'/fonts/overpass-bold-webfont.woff2',
-			'/fonts/overpass-italic-webfont.woff2'
-		])
-	}
-	e.waitUntil(preCache())
+	'/fonts/overpass/overpass-regular-webfont.woff2',
+	'/fonts/vollkorn/vollkorn-webfont.woff2',
+	'/fonts/overpass/overpass-semibold-webfont.woff2',
+	'/fonts/overpass/overpass-bold-webfont.woff2',
+	'/fonts/overpass/overpass-italic-webfont.woff2'
+]
+
+self.addEventListener('install', e => {
+	e.waitUntil(
+		caches.open('static').then(cache => {
+			return cache.addAll(REQUIRED_FILES)
+		}).then(() => {
+			return self.skipWaiting()
+		})
+	)
 })
 
-
 self.addEventListener('fetch', e => {
-	var request = e.request
-	return caches.open('static').then(cache => {
-		return cache.match(request).then(matching => {
-			return matching || fetch(request)
+	e.respondWith(
+		caches.match(e.request).then(res => {
+
+			if (res) {
+				return res
+			}
+
+			return fetch(e.request)
 		})
-	})
+	)
 })
